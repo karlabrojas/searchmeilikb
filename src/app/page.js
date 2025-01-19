625d47bc5b50771f0562f9dbe6419c2830023bda
Karlabrojas <karlabrojasr2@gmail.com>
2025-01-19 00:42:22 -0600

    Agregación de estilos

--- src/app/page.js
+++ src/app/page.js
@@ -28,22 +28,27 @@ export default function Home() {
 
   return (
     <div>
-      <div> 
+      <div className="flex justify-center content-center my-4"> 
         <input placeholder="Buscar" 
         value={buscarPeliculas}
         onChange={buscar}
-        type="text"/>
+        type="text"
+        className="w-3/4 bg-gray-100 h-12 p-4 rounded-lg "/>
       </div>
-      <div> 
+      <div className="grid grid-cols-3 m-10 bg-gray-50 " > 
         {
           resultados.map((movie) =>{
             return (
-              <div key ={movie.id} className="overflow-hidden shadow-lg my-6">
+              <div key ={movie.id} className=" bg-indigo-100 overflow-hidden shadow-2xl my-6  flex flex-row w-96 bg-blue-50  mx-10 h-80">
                 <img
                 src={movie.poster}
                 alt={movie.title}
+                width={200}
                 />
-                <h2>{movie.title}</h2>
+                <div>
+                <h2 className="text-2xl m-5 text-center font-mono font-semibold text-pink-600">{movie.title}</h2>
+                <p className="overflow-hidden m-4 text-sm">{movie.overview}</p>
+              </div>
               </div>
             )
           })
