8e0d95ae17e817ca01522b2f0b066bd575d0979a
Karlabrojas <karlabrojasr2@gmail.com>
2025-01-18 22:58:35 -0600

    Configuracion inicial de meilisearch

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,37 @@
 import Image from "next/image";
 import { MeiliSearch } from "meilisearch";
+import { useState } from "react";
 
 export default function Home() {
-  
+  const [resultados, setResultados] = useState ([]); 
+
   const client = new MeiliSearch({
     host: 'http://127.233.129.212',
     apiKey: '9674910d2440954f136b0e1537007301972088cd71ca00c44008f4a9a024',
   })
 
+  const buscar = async(e) => {
+    client.index("movies").search(e.target.value).then((resultado => setResultados(resultado.hits)))
+  }
 
   return (
     <div>
       <div> 
-        <input placeholder="Buscar" />
+        <input placeholder="Buscar" onChange={buscar}/>
+      </div>
+      <div> 
+        {
+          resultados.map((index) =>{
+            return (
+              <div key ={index.id}>
+                <img
+                src="index.poster
+                "/>
+                <h2>index.title</h2>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   );
