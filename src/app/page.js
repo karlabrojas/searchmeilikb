1f57b1016f3357926be588d6a83816f91337787f
Karlabrojas <karlabrojasr2@gmail.com>
2025-01-18 23:55:01 -0600

    Correción de errores en la funcion buscar

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,49 @@
+"use client"; 
+
+
 import Image from "next/image";
 import { MeiliSearch } from "meilisearch";
 import { useState } from "react";
 
+const client = new MeiliSearch({
+  host: 'http://172.233.129.212/',
+  apiKey: '9674910d2440954f136b0e1537007301972088cd71ca00c44008f4a9a024',
+})
+
+
 export default function Home() {
+  const [buscarPeliculas, setBuscarPeliculas] = useState ("")
   const [resultados, setResultados] = useState ([]); 
 
-  const client = new MeiliSearch({
-    host: 'http://127.233.129.212',
-    apiKey: '9674910d2440954f136b0e1537007301972088cd71ca00c44008f4a9a024',
-  })
 
   const buscar = async(e) => {
-    client.index("movies").search(e.target.value).then((resultado => setResultados(resultado.hits)))
+    const query = e.target.value;
+    setBuscarPeliculas(query)
+    if(query){
+      const results = await client.index("movies").search(query);setResultados(results.hits);
+    } else {
+      setResultados([])
+    }
   }
 
   return (
     <div>
       <div> 
-        <input placeholder="Buscar" onChange={buscar}/>
+        <input placeholder="Buscar" 
+        value={buscarPeliculas}
+        onChange={buscar}
+        type="text"/>
       </div>
       <div> 
         {
-          resultados.map((index) =>{
+          resultados.map((movie) =>{
             return (
-              <div key ={index.id}>
+              <div key ={movie.id} className="overflow-hidden shadow-lg my-6">
                 <img
-                src="index.poster
-                "/>
-                <h2>index.title</h2>
+                src={movie.poster}
+                alt={movie.title}
+                />
+                <h2>{movie.title}</h2>
               </div>
             )
           })
